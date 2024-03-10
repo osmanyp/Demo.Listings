de2db25cea94adbf445151bac8bdc82e9b0902ef
Osmany Perez Feria <osmanyperezferia@osmanys-air.lan>
2024-03-10 12:10:47 -0400

    Adding listings repo with db context.

--- Demo.Listings.Infrastructure/DataAccess/EF/Repositories/ListingsRepository.cs
+++ Demo.Listings.Infrastructure/DataAccess/EF/Repositories/ListingsRepository.cs
@@ -2,6 +2,11 @@ namespace Demo.Listings.Infrastructure.DataAccess.EF
 {
     internal class ListingsRepository
     {
-        
+        private readonly ListingsDbContext _listingsDbContext;
+
+        public ListingsRepository(ListingsDbContext listingsDbContext)
+        {
+            _listingsDbContext = listingsDbContext;
+        }
     }
 }
